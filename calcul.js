f099b81516b8db39fefbd180958de77611937587
Volcasaurus <155455967+Volcasaurus@users.noreply.github.com>
2024-08-05 12:36:50 +0200

    Add files via upload

---
+++ calcul.js
@@ -0,0 +1,450 @@
+class PersoDofus {
+    static Difficulty;
+    static Tank;
+    static Solo;
+    static Team;
+    static Early;
+    static Static;
+    static Dynamic;
+    static Polyvalence;
+    static imagePath;
+    static Mismatch = 0;
+}
+
+class Cra extends PersoDofus {
+    static Difficulty = -5;
+    static Tank = -5;
+    static DamageDealer = 5;
+    static Solo = 4;
+    static Team = 3;
+    static Early = 5;
+    static Static = 5;
+    static Dynamic = 1;
+    static Polyvalence = -5;
+    static imagePath = './Cra.png';
+    static Mismatch = 0;
+}
+
+class Eca extends PersoDofus {
+    static Difficulty = 0;
+    static Tank = 0;
+    static DamageDealer = 3;
+    static Solo = 4;
+    static Team = 0;
+    static Early = 3;
+    static Static = 3;
+    static Dynamic = 3;
+    static Polyvalence = 5;
+    static imagePath = './Ecaflip.png';
+    static Mismatch = 0;
+}
+
+class Elio extends PersoDofus {
+    static Difficulty = 5;
+    static Tank = -4;
+    static DamageDealer = -3;
+    static Solo = 5;
+    static Team = 5;
+    static Early = 4;
+    static Static = 2;
+    static Dynamic = 5;
+    static Polyvalence = 3;
+    static imagePath = './Eliotrope.png';
+    static Mismatch = 0;
+}
+
+class Eni extends PersoDofus {
+    static Difficulty = -4;
+    static Tank = 3;
+    static DamageDealer = -5;
+    static Solo = 1;
+    static Team = 5;
+    static Early = 5;
+    static Static = 5;
+    static Dynamic = 2;
+    static Polyvalence = -3;
+    static imagePath = './Eniripsa.png';
+    static Mismatch = 0;
+}
+
+class Enu extends PersoDofus {
+    static Difficulty = -2;
+    static Tank = -5;
+    static DamageDealer = -4;
+    static Solo = 0;
+    static Team = 4;
+    static Early = 5;
+    static Static = 5;
+    static Dynamic = 1;
+    static Polyvalence = -5;
+    static imagePath = './Enutrof.png';
+    static Mismatch = 0;
+}
+
+class Feca {
+    static Difficulty = -5;
+    static Tank = 4;
+    static DamageDealer = -2;
+    static Solo = 5;
+    static Team = 4;
+    static Early = 5;
+    static Static = 5;
+    static Dynamic = 4;
+    static Polyvalence = 4;
+    static imagePath = './Feca.png';
+    static Mismatch = 0;
+}
+
+class Forge extends PersoDofus {
+    static Difficulty = -1;
+    static Tank = 0;
+    static DamageDealer = 3;
+    static Solo = 3;
+    static Team = 3;
+    static Early = 4;
+    static Static = 2;
+    static Dynamic = 3;
+    static Polyvalence = 5;
+    static imagePath = './Forgelance.png';
+    static Mismatch = 0;
+}
+
+class Hupper extends PersoDofus {
+    static Difficulty = 3;
+    static Tank = -1;
+    static DamageDealer = 0;
+    static Solo = 5;
+    static Team = 0;
+    static Early = 3;
+    static Static = 3;
+    static Dynamic = 5;
+    static Polyvalence = 5;
+    static imagePath = './Huppermage.png';
+    static Mismatch = 0;
+}
+
+class Iop extends PersoDofus {
+    static Difficulty = -1;
+    static Tank = 3;
+    static DamageDealer = 5;
+    static Solo = 2;
+    static Team = 4;
+    static Early = 3;
+    static Static = 3;
+    static Dynamic = 3;
+    static Polyvalence = -3;
+    static imagePath = './Iop.png';
+    static Mismatch = 0;
+}
+
+class Ougi extends PersoDofus {
+    static Difficulty = -5;
+    static Tank = 5;
+    static DamageDealer = 5;
+    static Solo = 2;
+    static Team = 3;
+    static Early = 5;
+    static Static = 5;
+    static Dynamic = 0;
+    static Polyvalence = -5;
+    static imagePath = './Ouginak.png';
+    static Mismatch = 0;
+}
+
+class Osa extends PersoDofus {
+    static Difficulty = 4;
+    static Tank = 1;
+    static DamageDealer = 0;
+    static Solo = 3;
+    static Team = 0;
+    static Early = -5;
+    static Static = 4;
+    static Dynamic = 3;
+    static Polyvalence = 4;
+    static imagePath = './Osamodas.png';
+    static Mismatch = 0;
+}
+
+class Panda extends PersoDofus {
+    static Difficulty = 1;
+    static Tank = 2;
+    static DamageDealer = -3;
+    static Solo = 3;
+    static Team = 5;
+    static Early = 5;
+    static Static = 5;
+    static Dynamic = 5;
+    static Polyvalence = 4;
+    static imagePath = './Pandawa.png';
+    static Mismatch = 0;
+}
+
+class Roublard extends PersoDofus {
+    static Difficulty = 5;
+    static Tank = -4;
+    static DamageDealer = 5;
+    static Solo = 4;
+    static Team = 3;
+    static Early = -5;
+    static Static = 1;
+    static Dynamic = 4;
+    static Polyvalence = -5;
+    static imagePath = './Roublard.png';
+    static Mismatch = 0;
+}
+
+class Sacri extends PersoDofus {
+    static Difficulty = -1;
+    static Tank = 5
+    static DamageDealer = 2;
+    static Solo = 4;
+    static Team = 3;
+    static Early = -4;
+    static Static = 5;
+    static Dynamic = 5;
+    static Polyvalence = -3;
+    static imagePath = './Sacrieur.png';
+    static Mismatch = 0;
+}
+
+class Sadi extends PersoDofus {
+    static Difficulty = 3;
+    static Tank = -4;
+    static DamageDealer = 3;
+    static Solo = 4;
+    static Team = 4;
+    static Early = -3;
+    static Static = 1;
+    static Dynamic = 5;
+    static Polyvalence = -4;
+    static imagePath = './Sadida.png';
+    static Mismatch = 0;
+}
+
+class Sram extends PersoDofus {
+    static Difficulty = 3;
+    static Tank = -5;
+    static DamageDealer = 5;
+    static Solo = 5;
+    static Team = 0;
+    static Early = -3;
+    static Static = 0;
+    static Dynamic = 5;
+    static Polyvalence = -5;
+    static imagePath = './Sram.png';
+    static Mismatch = 0;
+}
+
+class Steamer extends PersoDofus {
+    static Difficulty = 2;
+    static Tank = -3;
+    static DamageDealer = 0;
+    static Solo = 4;
+    static Team = 4;
+    static Early = -1;
+    static Static = 4;
+    static Dynamic = 5;
+    static Polyvalence = 5;
+    static imagePath = './Steamer.png';
+    static Mismatch = 0;
+}
+
+class Xelor extends PersoDofus {
+    static Difficulty = 5;
+    static Tank = -5;
+    static DamageDealer = 4;
+    static Solo = 4;
+    static Team = 3;
+    static Early = -1;
+    static Static = 0;
+    static Dynamic = 5;
+    static Polyvalence = -5;
+    static imagePath = './Xelor.png';
+    static Mismatch = 0;
+}
+
+class Zobal extends PersoDofus {
+    static Difficulty = -4;
+    static Tank = 5;
+    static DamageDealer = -5;
+    static Solo = 0;
+    static Team = 5;
+    static Early = 5;
+    static Static = 5;
+    static Dynamic = 2;
+    static Polyvalence = -4;
+    static imagePath = './Zobal.png';
+    static Mismatch = 0;
+}
+
+const classRegistry = [
+    Cra,
+    Eca,
+    Elio,
+    Eni,
+    Enu,
+    Feca,
+    Forge,
+    Hupper,
+    Iop,
+    Ougi,
+    Osa,
+    Panda,
+    Roublard,
+    Sacri,
+    Sadi,
+    Sram,
+    Steamer,
+    Xelor,
+    Zobal,
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('submit-button');
+    button.addEventListener('click', CalculateClasses);
+});
+
+function CalculateClasses()
+{
+    const val1 = document.getElementById('1').value;
+    const val2 = document.getElementById('2').value;
+    const val3 = document.getElementById('3').value;
+    const val4 = document.getElementById('4').value;
+    const val5 = document.getElementById('5').value;
+    const val6 = document.getElementById('6').value;
+    const val7 = document.getElementById('7').value;
+
+    // Add error between slider value and actual class value
+    classRegistry.forEach(ClassRef => {
+        let delta = Math.abs(val1 - ClassRef.Difficulty);
+        ClassRef.Mismatch += delta * 4;
+    });
+
+    classRegistry.forEach(ClassRef => {
+        let delta = Math.abs(val2 - ClassRef.Tank);
+        ClassRef.Mismatch += delta * 6;
+    });
+
+    classRegistry.forEach(ClassRef => {
+        let delta = Math.abs(val4 - ClassRef.Early);
+        ClassRef.Mismatch += delta * 4;
+    });
+
+    classRegistry.forEach(ClassRef => {
+        let delta = Math.abs(val6 - ClassRef.Polyvalence);
+        ClassRef.Mismatch += delta * 3;
+    });
+
+    classRegistry.forEach(ClassRef => {
+        let delta = Math.abs(val7 - ClassRef.DamageDealer);
+        ClassRef.Mismatch += delta * 4;
+    });
+
+    // Evaluate multiple play styles
+    classRegistry.forEach(ClassRef => {
+        // If user undecided: just take the average viability in solo and team and subtract it from 5 -> "best case": 5 - 5 -> no delta added (x8 is in reality x4, doubling because of the shorter range)
+        if (val3 == 0) {
+            let delta = (5 - (ClassRef.Solo + ClassRef.Team) / 2)
+            ClassRef.Mismatch += delta * 8;
+        }
+        // Check if the class is good enough relative to the value expected in the slider, if it's good enough -> delta 0 negative, so nothing's added
+        else if (val3 > 0) {
+            let delta = val3 - ClassRef.Team;
+            if (delta > 0) ClassRef.Mismatch += delta * 8;
+        }
+        else if (val3 < 0) {
+            let delta = val3 - ClassRef.Solo;
+            if (delta > 0) ClassRef.Mismatch += delta * 8;
+        }
+    });
+
+    // Evaluate multiple play styles
+    classRegistry.forEach(ClassRef => {
+        // If user undecided: just take the average viability in solo and team and subtract it from 5 -> "best case": 5 - 5 -> no delta added
+        if (val5 == 0) {
+            let delta = (5 - (ClassRef.Dynamic + ClassRef.Static) / 2)
+            ClassRef.Mismatch += delta*4;
+        }
+        // Check if the class is good enough relative to the value expected in the slider, if it's good enough -> delta 0 negative, so nothing's added
+        else if (val5 > 0) {
+            let delta = val5 - ClassRef.Dynamic;
+            if (delta > 0) ClassRef.Mismatch += delta * 4;
+        }
+        else if (val5 < 0) {
+            let delta = val5 - ClassRef.Static;
+            if (delta > 0) ClassRef.Mismatch += delta * 4;
+        }
+    });
+
+    const classValues = classRegistry.map(ClassRef => ({
+        class: ClassRef,
+        Mismatch: ClassRef.Mismatch
+    }));
+
+    classValues.sort((a, b) => a.Mismatch - b.Mismatch);
+
+    const top3Smallest = classValues.slice(0, 3);
+    top3Smallest.forEach(item => {
+        console.log(`${item.class.name} with value ${item.Mismatch}`);
+    });
+
+    // Prepare new page
+    var main = document.getElementById("main");
+    Array.from(main.children).forEach(child => child.style.display = "none");
+    var results = document.getElementById("results");
+    results.style.display = "grid";
+
+    // 2x3 grid: fix medals in the left column, class images in the right column
+    var img1 = document.createElement("img");
+    img1.src = './Classe1.png';
+    results.appendChild(img1);
+
+    var img2 = document.createElement("img");
+    img2.src = top3Smallest[0].class.imagePath;
+    img2.style.width = "400%";
+    img2.style.height = "auto";
+    results.appendChild(img2);
+
+    var img3 = document.createElement("img");
+    img3.src = './Classe2.png';
+    results.appendChild(img3);
+
+    var img4 = document.createElement("img");
+    img4.src = top3Smallest[1].class.imagePath;
+    img4.style.width = "400%";
+    img4.style.height = "auto";
+    results.appendChild(img4);
+
+    var img5 = document.createElement("img");
+    img5.src = './Classe3.png';
+    results.appendChild(img5);
+
+    var img6 = document.createElement("img");
+    img6.src = top3Smallest[2].class.imagePath;
+    img6.style.width = "400%";
+    img6.style.height = "auto";
+    results.appendChild(img6);
+
+    var button = document.createElement('button');
+    button.textContent = 'Retour';
+    button.style.padding = '10px 20px';
+    button.style.backgroundColor = 'green';
+    button.style.color = 'white';
+    button.style.border = 'none';
+    button.style.borderRadius = '4px';
+    button.style.cursor = 'pointer';
+    button.style.fontSize = '16px';
+    button.style.marginTop = '20px';
+    results.appendChild(button);
+    button.addEventListener('click', restart);
+
+    // Reset at the very end
+    classRegistry.forEach(ClassRef => {
+        ClassRef.Mismatch = 0;
+    });
+}
+
+function restart() {
+    Array.from(main.querySelectorAll('input')).forEach(input => input.value = 0);
+    location.reload();
+}
